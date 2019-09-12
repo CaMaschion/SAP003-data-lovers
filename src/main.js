e274a08d9a30de09f5734b5d450ada6072920dec
elaineshimabukuro <laineyoshizato@gmail.com>
2019-09-12 17:49:03 -0300

    testes

---
+++ src/main.js
@@ -0,0 +1,13 @@
+//const pokemonNames = POKEMON.pokemon
+
+//for (let pokemons in POKEMON) {
+//let name = pokemonNames.map(pokemon.name => `${pokemon.name}`);
+ //console.log(POKEMON.name);
+//}
+
+const POKEMONS = POKEMON.pokemon;
+
+
+for (let poke of POKEMONS){
+    document.write(`${poke.name}</br>`)
+    }
